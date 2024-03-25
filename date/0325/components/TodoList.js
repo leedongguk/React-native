89172b52790936f22e3938de1f0ab0411b10d4e4
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2024-03-25 17:09:02 +0900

    Add files via upload

---
+++ date/0325/components/TodoList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {FlatList, View, Text, StyleSheet} from 'react-native';
+import TodoItem from './TodoItem';
+
+function  TodoList({todos, onToggle}){
+    return (
+        <FlatList 
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        style={styles.list}
+        data={todos}
+        renderItem={({item}) => (
+            <TodoItem id={item.id} text={item.text} done={item.done} 
+            onToggle={onToggle}/>
+        )}
+        keyExtractor={item => item.id.toString()} />
+    );
+}
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+    },
+});
+
+export default TodoList;
